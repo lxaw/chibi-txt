71a37a0fe6df36f7626928eaea031e88af1bbcc5
lxaw <whalenlex@gmail.com>
2023-08-19 14:41:33 -0400

    made the huff tree

--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,113 @@
-use core::fmt;
-use std::{collections::BTreeMap};
+use std::collections::{BinaryHeap,BTreeMap};
+use std::{cmp::Reverse};
 
 // https://www.geeksforgeeks.org/huffman-coding-greedy-algo-3/
 
-#[derive(Clone, Copy)]
-struct HeapNode {
+type Link = Option<Box<Node>>;
+
+/***************
+NODE begin
+ */
+#[derive(Debug,Clone)]
+struct Node{
     data: char,
     freq: usize,
+    l: Link,
+    r: Link,
 }
-
-impl fmt::Debug for HeapNode{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "HeapNode [{}:{}]", self.data, self.freq)
-    }
-}
-
-impl PartialEq for HeapNode{
+impl PartialEq for Node{
     fn eq(&self, other: &Self) -> bool {
         self.freq == other.freq
     }
 }
-impl PartialOrd for HeapNode{
-    fn ge(&self, other: &Self) -> bool {
-        self.freq >= other.freq
-    }
-    fn gt(&self, other: &Self) -> bool {
-        self.freq > other.freq
-    }
-    fn le(&self, other: &Self) -> bool {
-        self.freq <= other.freq 
-    }
-    fn lt(&self, other: &Self) -> bool {
-        self.freq < other.freq 
-    }
-
+impl PartialOrd for Node{
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        match self.data.partial_cmp(&other.data) {
-            Some(core::cmp::Ordering::Equal) => {}
-            ord => return ord,
-        }
         self.freq.partial_cmp(&other.freq)
     }
+}
+impl Eq for Node{
 
 }
-
-struct Heap{
-    nodes : Vec<HeapNode>    
-}
-impl fmt::Debug for Heap{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f,"{:?}",self.nodes)
+impl Ord for Node{
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.freq.cmp(&other.freq)
     }
 }
-impl Heap{
-    // build the heap
-    fn build_heap(&mut self){
-        // index of last non-leaf node
-        let start_index = self.nodes.len()/2;
-
-        for i in (0..start_index).rev(){
-            self.heapify(i);
-        }
-    }
-    // heapify
-    fn heapify(&mut self,i:usize){
-        let mut largest = i;
-        let l = Heap::left_index(i);
-        let r = Heap::right_index(i);
-
-        // if left child larger than root
-        if l < self.nodes.len()-1 && self.nodes[l] > self.nodes[largest]{
-            largest = l;
-        }
-        // if right child larger than largest so far
-        if r < self.nodes.len()-1 && self.nodes[r] > self.nodes[largest]{
-            largest = r;
-        }
-        // if largest is not root
-        if largest != i{
-            self.nodes.swap(i,largest);
-            self.heapify(largest);
-        }
-    }
-    fn is_leaf(&self, pos:usize) -> bool{
-        pos*2 > self.nodes.len()-1
-    }
-    fn min_heapify(&mut self, pos:usize){
-        if !self.is_leaf(pos){
-            if self.nodes[pos] > self.nodes[Heap::left_index(pos)] ||
-                self.nodes[pos] > self.nodes[Heap::right_index(pos)]
-                {
-                    if self.nodes[Heap::left_index(pos)] < self.nodes[Heap::right_index(pos)]{
-                        self.nodes.swap(pos, Heap::left_index(pos));
-                        self.min_heapify(Heap::left_index(pos));
-                    }
-                    else{
-                        self.nodes.swap(pos,Heap::right_index(pos));
-                        self.min_heapify(Heap::right_index(pos));
-                    }
-                }
-        }
-    }
-    fn min_heap(&mut self){
-        for i in (0..(self.nodes.len())/2).rev(){
-            self.min_heapify(i);
-        }
-    }
-    fn left_index(index: usize) -> usize{
-        2*index
-    }
-    fn right_index(index: usize) -> usize{
-        (2*index) + 1
+impl Node{
+    fn new(data: char, freq: usize)->Node{
+        Node {data:data,freq:freq,l:None,r:None}
     }
 }
 
+/***************
+NODE END
+ */
+
+
+fn build_huff_tree(nodes : &mut Vec<Node>) -> Node{
+    // alg:
+    // 1) Order the nodes based on freq
+    // 2) Merge the two nodes with smallest freqs
+    // 3) repeat until one node left
+    let mut root = Node::new('$',0);
+
+    while(nodes.len() > 1){
+        let new_freq = nodes[0].freq + nodes[1].freq;
+        let left_link = Some(Box::new(Node{
+            data:nodes[0].data,freq:nodes[0].freq,
+            l:nodes[0].l.clone(),r:nodes[0].r.clone()
+        }));
+        let right_link = Some(Box::new(Node{
+            data:nodes[1].data,freq:nodes[1].freq,
+            l:nodes[1].l.clone(),r:nodes[1].r.clone()
+        }));
+        // $ is special character
+        let new_node = Node{data:'$',freq:new_freq,
+            l:left_link,r:right_link
+        };
+
+        // remove first two elements of vector
+        nodes.drain(0..2);
+
+        // push new node in 
+        nodes.push(new_node);
+        // sort the tree
+        nodes.sort();
+    }
+
+    root
+}
+
+fn get_huff_codes(huff_tree_head:Node) -> BTreeMap<Node,u32>{
+    let mut ret_hash = BTreeMap::new();
+
+
+
+    ret_hash
+}
+
+fn main() {
+    // let msg = "ABBAABABDA".to_string();
+
+    // let hm = get_hash_char_freq(msg);
+
+    // let nodes = get_nodes(hm);
+    let nodeA =  Node::new('A',35);
+    let nodeB =  Node::new('B',10);
+    let nodeC =  Node::new('C',20);
+    let nodeD =  Node::new('D',20);
+    let node_ =  Node::new('_',15);
+    let arr_nodes = [
+        nodeA,nodeB,nodeC,nodeD,node_
+    ];
+    let mut nodes = Vec::from(arr_nodes);
+    // sort in desc order
+    nodes.sort();
+
+    let tree_head = build_huff_tree(&mut nodes);
+
+    let huff_codes = get_huff_codes(tree_head);
+}
 
 fn get_hash_char_freq(msg:String) -> BTreeMap<char,usize> {
     /*
@@ -140,45 +134,15 @@ fn get_hash_char_freq(msg:String) -> BTreeMap<char,usize> {
     ret_hash
 }
 
-fn get_heap_nodes(btm: BTreeMap<char,usize>) -> Vec<HeapNode>{
-    let mut vec : Vec<HeapNode> = Vec::new();
+fn get_nodes(btm: BTreeMap<char,usize>) -> Vec<Node>{
+    let mut vec : Vec<Node> = Vec::new();
 
     for (c,f) in btm.iter(){
-        let node : HeapNode = HeapNode { data: (*c), freq: (*f) };
+        let node : Node = Node::new(*c,*f);
         vec.push(node);
     }
+
+    vec.sort();
+
     vec
-}
-
-fn main() {
-    let msg = "Hello world this is my message.".to_string();
-
-    let hm = get_hash_char_freq(msg);
-    let _vec = get_heap_nodes(hm);
-
-    let test_arr = [
-        HeapNode{data:'c',freq:20},
-        HeapNode{data:'c',freq:25},
-        HeapNode{data:'c',freq:10},
-        HeapNode{data:'c',freq:99},
-        HeapNode{data:'c',freq:60},
-        HeapNode{data:'c',freq:70},
-        HeapNode{data:'c',freq:82},
-        HeapNode{data:'c',freq:30},
-        HeapNode{data:'c',freq:50},
-        HeapNode{data:'c',freq:90},
-        HeapNode{data:'c',freq:58},
-        HeapNode{data:'c',freq:85},
-        HeapNode{data:'c',freq:71},
-        // need extra space here! TODO
-    ];
-    // 10, 20, 25, 60, 30, 58, 71, 99, 70, 82, 50, 90, 85
-
-    let nodes : Vec<HeapNode> = Vec::from(test_arr);
-
-    let mut heap : Heap = Heap { nodes: (nodes) };
-    heap.build_heap();
-    heap.min_heap();
-
-    println!("{:?}",heap);
 }
